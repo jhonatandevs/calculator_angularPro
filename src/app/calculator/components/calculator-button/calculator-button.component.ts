a478b3fc19c36336b2b931874f50becbbf6e3dae
JhonatanDev <jhonatan3506@gmail.com>
2025-04-01 21:36:46 -0500

    todos los botones fueron remplazados por el componente

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, input } from '@angular/core';
 
 @Component({
   selector: 'app-calculator-button',
@@ -11,6 +11,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     class:'w-1/4 border-r border-b border-indigo-400',
     attribute:'hola',
     'data-size':'XL'
-  }
+  },
+
 })
-export class CalculatorButtonComponent { }
+export class CalculatorButtonComponent {
+  public isCommand = input(false,{
+    transform:(value: boolean | string)=> typeof value=== 'string'?value==='':value
+  });
+  public isDouble = input(false,{
+    transform:(value: boolean | string)=> typeof value=== 'string'?value==='':value
+  });
+  @HostBinding('class.bg-indigo-700') get commandStyle(){
+    return this.isCommand();
+  }
+  @HostBinding('class.w-2/4') get commandStyleDouble(){
+    return this.isDouble();
+  }
+ }
